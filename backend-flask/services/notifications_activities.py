f3cf1099afecb96963961481f155e051f1d8593b
Mukul Anand <mukulanand.biz@gmail.com>
2024-04-27 12:09:27 +0000

    I didn't found anything problem maybe aws-xray configuration Can you tell me?

--- backend-flask/services/notifications_activities.py
+++ backend-flask/services/notifications_activities.py
@@ -1,32 +1,27 @@
-from aws_xray_sdk.core import xray_recorder
+
 from datetime import datetime, timedelta, timezone
 class NotificationsActivities:
   def run():
-    with xray_recorder.begin_segment('notifications_activities') as segment:
-      now = datetime.now(timezone.utc).astimezone()
-      dict={
-        "now": now.isoformat()
-      }
-      segment.put_metadata('key', dict, 'namespace')
-      results = [{
-        'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-        'handle':  'coco',
-        'message': 'I am white unicorn',
-        'created_at': (now - timedelta(days=2)).isoformat(),
-        'expires_at': (now + timedelta(days=5)).isoformat(),
-        'likes_count': 5,
-        'replies_count': 1,
+    now = datetime.now(timezone.utc).astimezone()
+    results = [{
+      'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+      'handle':  'coco',
+      'message': 'I am white unicorn',
+      'created_at': (now - timedelta(days=2)).isoformat(),
+      'expires_at': (now + timedelta(days=5)).isoformat(),
+      'likes_count': 5,
+      'replies_count': 1,
+      'reposts_count': 0,
+      'replies': [{
+        'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
+        'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+        'handle':  'worf',
+        'message': 'this post has no honor!',
+        'likes_count': 0,
+        'replies_count': 0,
         'reposts_count': 0,
-        'replies': [{
-          'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
-          'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-          'handle':  'worf',
-          'message': 'this post has no honor!',
-          'likes_count': 0,
-          'replies_count': 0,
-          'reposts_count': 0,
-          'created_at': (now - timedelta(days=2)).isoformat()
-        }],
-      }
-      ]
-      return results
+        'created_at': (now - timedelta(days=2)).isoformat()
+      }],
+    }
+    ]
+    return results
